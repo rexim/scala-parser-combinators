9d0fd58b3241c461b1dba8db02f2ffd768283652
rexim <reximkut@gmail.com>
2013-06-30 11:20:06 +0700

    Library: renamed implicit conversions of CharParser.

--- sources/Parsers.scala
+++ sources/Parsers.scala
@@ -2,7 +2,7 @@ import scala.language.implicitConversions
 import scala.language.postfixOps
 
 trait CharParsers {
-  implicit def Char2CharParser(x: Char): Parser[Char] = new Parser[Char] {
+  implicit def OneLetter(x: Char): Parser[Char] = new Parser[Char] {
     override def apply(input: String) = {
       val trimmedInput = input.dropWhile(_.isSpaceChar)
       if(trimmedInput.isEmpty) {
@@ -15,7 +15,7 @@ trait CharParsers {
     }
   }
 
-  implicit def String2CharParser(xs: String): Parser[Char] =
+  implicit def OneOfLetters(xs: String): Parser[Char] =
     if(!xs.isEmpty) {
       xs.tail.foldLeft(xs.head: Parser[Char]) {
         case (acc, x) => acc | x
