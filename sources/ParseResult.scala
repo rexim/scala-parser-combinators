18e1593f7421f9bdd656f6f71ec5c2fc8def0a35
rexim <reximkut@gmail.com>
2013-07-07 17:02:58 +0700

    Library: added documentation for ParseResult trait and its cases.

--- sources/ParseResult.scala
+++ sources/ParseResult.scala
@@ -1,4 +1,14 @@
 
+/** A trait for parser results. A result is either successful or not.
+  */
 trait ParseResult[+T]
+
+/** The success case of `ParseResult`: contains the result and the
+  * remaining input.
+  */
 case class Success[+T](value: T, input: String) extends ParseResult[T]
+
+/** The failure case of `ParseResult`: contains an error-message and
+  * the remaining input.
+  */
 case class Failure[+T](message: String, input: String) extends ParseResult[T]
